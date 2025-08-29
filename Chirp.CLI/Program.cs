a5c7c42495d2ff62d30f69bc632b62dfce21f10d
akdy1234 <akdy@itu.dk>
2025-08-29 10:56:18 +0200

    Add files via upload

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,7 @@
+﻿List<string> cheeps = new() {"Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer.", "Test2", "test3"};
+
+foreach (var cheep in cheeps)
+{
+    Console.WriteLine(cheep);
+    Thread.Sleep(1000);
+}

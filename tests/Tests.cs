12151cb6b684042126135790af5b3ac5fd232a12
AkselDyhr <akdy@itu.dk>
2025-10-08 16:52:20 +0200

    2 Unit tests made, code refactored for better structure, website still functional.

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1,6 +1,73 @@
-namespace Testing.Tests;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 
+namespace MyChat.Razor.Model;
+
+
+using Xunit;
 public class Tests
 {
-    
+	[Fact]
+	public void UnitUserCreatingTest()
+	{
+
+		var userId = 12345;
+		var username = "Tester";
+		var email = "Tester@email.com";
+		
+		var user = new User
+		{
+			Id = userId,
+			Username = username,
+			Email = email
+		};
+	
+		Assert.Equal(user.Id, userId);
+		Assert.Equal(user.Username, username);
+		Assert.Equal(user.Email, email);
+		
+		Assert.Equal(user.Id, 12345);
+		Assert.Equal(user.Username, "Tester");
+		Assert.Equal(user.Email, "Tester@email.com");
+	}
+	
+	[Fact]
+	public void UnitMessageCreatingTest()
+	{
+
+		var userId = 12345;
+		var username = "Tester";
+		var email = "Tester@email.com";
+		
+		var user = new User
+		{
+			Id = userId,
+			Username = username,
+			Email = email
+		};
+
+		var id = 99999;
+		var content = "Jeg hopper fra femte!";
+		var sentAt = DateTime.Now;
+		
+		var message = new Message
+		{
+			Id = id,
+			Content = content,
+			SentAt = sentAt, 
+			UserId = userId,
+			User = user
+		};
+		
+		
+		
+		Assert.Equal(message.Id, id);
+		Assert.Equal(message.Content, content);
+		Assert.Equal(message.SentAt, sentAt);
+		Assert.Equal(message.UserId, userId);
+		Assert.Equal(message.User, user);
+		
+		Assert.Equal(message.Id, 99999);
+		Assert.Equal(message.Content, "Jeg hopper fra femte!");
+		Assert.Equal(message.UserId, 12345);
+	}
 }

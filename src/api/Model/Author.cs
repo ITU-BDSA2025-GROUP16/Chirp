c6f14355ed0528882c4f8289f6257e99973d5878
Torkilgit <tlirok03@gmail.com>
2025-10-22 15:37:00 +0200

    Warnings addressed.

--- src/api/Model/Author.cs
+++ src/api/Model/Author.cs
@@ -3,9 +3,9 @@
 public class Author
 {
     public int AuthorId { get; set; }  // EF Core will consider this the primary key by convention
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 
     // Navigation property
-    public ICollection<Cheep> Cheeps { get; set; }
+    public ICollection<Cheep> Cheeps { get; set; } = new List<Cheep>();
 }

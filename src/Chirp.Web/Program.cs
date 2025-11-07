fc0744d657861ec9b500e015de66d9171cb31e30
PerkyMan227 <shes@itu.dk>
2025-11-07 13:40:27 +0100

    Removed hardcoded url for azure deployment.
    Moved local development settings to launchSettings.json

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -19,8 +19,6 @@ builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 builder.Services.AddRazorPages();
 
-builder.WebHost.UseUrls("http://localhost:5696");
-
 // Session configuration
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();

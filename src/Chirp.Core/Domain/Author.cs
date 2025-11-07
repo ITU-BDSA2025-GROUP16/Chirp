902e3694ef66a65b4373a4490bc4cfd3dd7ad3e4
Victor <visv@itu.dk>
2025-11-07 12:08:24 +0100

    When registering you get your Author.name set as your Author.Email.

--- src/Chirp.Core/Domain/Author.cs
+++ src/Chirp.Core/Domain/Author.cs
@@ -8,14 +8,22 @@ public class Author : IdentityUser<int>
 
     private string _email = string.Empty;
     
-    public override string? Email 
-    { 
-        get => _email; 
-        set 
-        { 
+    public override string? Email
+    {
+        get => _email;
+        set
+        {
             _email = value ?? string.Empty;
-            UserName = value; // Automatically set UserName when Email is set
-        } 
+
+            // Set UserName to the email
+            UserName = _email;
+
+            // If Name is empty, also set it to the email
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = _email;
+            }
+        }
     }
     
 

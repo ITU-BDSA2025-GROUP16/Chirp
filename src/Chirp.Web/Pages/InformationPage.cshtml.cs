41dcaf6649970a7f8712b1a0ba393eeaa74820af
aksel <akdy@itu.dk>
2025-11-28 12:37:23 +0100

    information page shows own cheeps

--- src/Chirp.Web/Pages/InformationPage.cshtml.cs
+++ src/Chirp.Web/Pages/InformationPage.cshtml.cs
@@ -3,21 +3,30 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Chirp.Core.Domain;
+using Chirp.Core.Services;
 
 namespace Chirp.Web.Pages;
 
 [Authorize]
 public class InformationPageModel : PageModel
 {
+        private readonly ICheepService _service;
+
     private readonly UserManager<Author> _userManager;
     
     public string Name { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     
+    public string? Author { get; set; } = string.Empty;
 
-    public InformationPageModel(UserManager<Author> userManager)
+    public int CurrentPage { get; set; } = 1;
+
+    public List<CheepViewModel> Cheeps { get; set; } = new();
+
+    public InformationPageModel(ICheepService service, UserManager<Author> userManager)
     {
+         _service = service;
         _userManager = userManager;
     }
 
@@ -33,5 +42,26 @@ public class InformationPageModel : PageModel
         Username = currentUser.UserName ?? string.Empty;
         Email = currentUser.Email ?? string.Empty;
 
+
+        //Cheeps
+        Author = User.Identity?.Name;
+
+        int pageNumber = 1;
+        string? pageQuery = HttpContext.Request.Query["page"];
+        if (!string.IsNullOrEmpty(pageQuery) && int.TryParse(pageQuery, out int parsedPage))
+        {
+            pageNumber = parsedPage > 0 ? parsedPage : 1;
+        }
+
+        CurrentPage = pageNumber;
+
+        if (string.IsNullOrEmpty(Author))
+        {
+            Cheeps = new();
+            return;
+        }
+
+        
+        Cheeps = _service.GetCheepsFromAuthor(Author, pageNumber);
     }
 }
